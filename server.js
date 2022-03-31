06db85f13db73ce87dbb181d446517eb987ef968
Oluka Isaac <hilghsilk2damson@gmail.com>
2022-03-31 16:06:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 corsProxy.createServer({
-    originWhitelist: ['http://localhost:5503', 'https://sendairtime.netlify.app/'],
+    originWhitelist: ['http://localhost:5503', 'https://sendairtime.netlify.app/' 'http://localhost:3000'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, () => {
