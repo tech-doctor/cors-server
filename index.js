daa60a79a8878f07038090d7cdd4e5c46183a462
Oluka Isaac <hilghsilk2damson@gmail.com>
2024-01-28 16:39:26 +0100

    updated

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 const corsProxy = require('cors-anywhere');
 
-// Listen on a specific host via the HOST environment variable.
-const host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable.
-const port = process.env.PORT || 8080;
+// // Listen on a specific host via the HOST environment variable.
+// const host = process.env.HOST || '0.0.0.0';
+// // Listen on a specific port via the PORT environment variable.
+// const port = process.env.PORT || 8080;
 
-corsProxy.createServer({
-    originWhitelist: ['http://localhost:5503', 'https://m-uber-clone.netlify.app', 'https://sendairtime.netlify.app/'],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, () => {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+// corsProxy.createServer({
+//     originWhitelist: ['http://localhost:5503', 'https://m-uber-clone.netlify.app', 'https://sendairtime.netlify.app/'],
+//     requireHeader: ['origin', 'x-requested-with'],
+//     removeHeaders: ['cookie', 'cookie2']
+// }).listen(port, host, () => {
+//     console.log('Running CORS Anywhere on ' + host + ':' + port);
+// }); 
+
+
+function createCorsProxyServer() {
+    // Listen on a specific host via the HOST environment variable.
+    const host = process.env.HOST || '0.0.0.0';
+    // Listen on a specific port via the PORT environment variable.
+    const port = process.env.PORT || 8080;
+
+    return corsProxy.createServer({
+        originWhitelist: ['http://localhost:5503', 'https://m-uber-clone.netlify.app', 'https://sendairtime.netlify.app/'],
+        requireHeader: ['origin', 'x-requested-with'],
+        removeHeaders: ['cookie', 'cookie2']
+    }).listen(port, host, () => {
+        console.log('Running CORS Anywhere on ' + host + ':' + port);
+    });
+}
+
+
+module.exports = createCorsProxyServer();
+// ... (previous code) module.exports = app; // Export the Express app
